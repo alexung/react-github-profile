6c68090ddec2d74552f5089f6ba2de60a1cf54df
Alex Ung <aung@lyft.com>
2019-01-30 15:23:56 -0800

    package.json update

--- src/screens/user/components/query.js
+++ src/screens/user/components/query.js
@@ -1,110 +1,113 @@
-import { Component, useContext, useReducer, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import * as GitHub from '../../../github-client'
+import {useContext, useReducer, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import * as GitHub from '../../../github-client';
 
-function Query({ query, variables, children, normalize = data => data }) {
-  const client = useContext(Github.Context);
+function Query({query, variables, normalize = data => data, children}) {
+  const client = useContext(GitHub.Context);
   const [state, setState] = useReducer(
-    (state, newState) => ({ ...state, ...newState }),
-    { loaded: false, fetching: false, data: null, error: null } // this initializes state
+    (state, newState) => ({...state, ...newState}),
+    {loaded: false, fetching: false, data: null, error: null}, // this initializes state
   );
   // the callback below runs when component is mounted and whenever the 'query' or 'variable' change
-  useEffect(() => {
-    setState({ fetching: true })
-    client
-      .request(query, variables)
-      .then(res =>
-        setState({
-          data: normalize(res),
-          error: null,
-          loaded: true,
-          fetching: false,
-        }),
-      )
-      .catch(error =>
-        this.setState({
-          error,
-          data: null,
-          loaded: false,
-          fetching: false,
-        }),
-      )
-    // below, the array is essentially componentDidUpdate.
-    // If either 'query' or 'variables' change, then we can run this effect
-  }, [query, variables]);
+  useEffect(
+    () => {
+      setState({fetching: true});
+      client
+        .request(query, variables)
+        .then(res =>
+          setState({
+            data: normalize(res),
+            error: null,
+            loaded: true,
+            fetching: false,
+          }),
+        )
+        .catch(error =>
+          this.setState({
+            error,
+            data: null,
+            loaded: false,
+            fetching: false,
+          }),
+        );
+      // below, the array is essentially componentDidUpdate.
+      // If either 'query' or 'variables' change, then we can run this effect
+    },
+    [query, variables],
+  );
 
   return children(state); // state is var in closure
-};
+}
 
 Query.propTypes = {
   query: PropTypes.string.isRequired,
   variables: PropTypes.object,
   children: PropTypes.func.isRequired,
   normalize: PropTypes.func,
-}
+};
 
-class Query extends Component {
-  // static propTypes = {
-  //   query: PropTypes.string.isRequired,
-  //   variables: PropTypes.object,
-  //   children: PropTypes.func.isRequired,
-  //   normalize: PropTypes.func,
-  // }
-  // static defaultProps = {
-  //   normalize: data => data,
-  // }
-  // static contextType = GitHub.Context
+export default Query;
 
-  // state = {loaded: false, fetching: false, data: null, error: null}
+// class Query extends Component {
+// static propTypes = {
+//   query: PropTypes.string.isRequired,
+//   variables: PropTypes.object,
+//   children: PropTypes.func.isRequired,
+//   normalize: PropTypes.func,
+// }
+// static defaultProps = {
+//   normalize: data => data,
+// }
+// static contextType = GitHub.Context
 
-  // componentDidMount() {
-  //   this._isMounted = true
-  //   this.query()
-  // }
+// state = {loaded: false, fetching: false, data: null, error: null}
 
-  // componentDidUpdate(prevProps) {
-  //   if (
-  //     !isEqual(this.props.query, prevProps.query) ||
-  //     !isEqual(this.props.variables, prevProps.variables)
-  //   ) {
-  //     this.query()
-  //   }
-  // }
+// componentDidMount() {
+//   this._isMounted = true
+//   this.query()
+// }
 
-  // componentWillUnmount() {
-  //   this._isMounted = false
-  // }
+// componentDidUpdate(prevProps) {
+//   if (
+//     !isEqual(this.props.query, prevProps.query) ||
+//     !isEqual(this.props.variables, prevProps.variables)
+//   ) {
+//     this.query()
+//   }
+// }
 
-  // query() {
-  //   this.setState({fetching: true})
-  //   const client = this.context
-  //   client
-  //     .request(this.props.query, this.props.variables)
-  //     .then(res =>
-  //       this.safeSetState({
-  //         data: this.props.normalize(res),
-  //         error: null,
-  //         loaded: true,
-  //         fetching: false,
-  //       }),
-  //     )
-  //     .catch(error =>
-  //       this.safeSetState({
-  //         error,
-  //         data: null,
-  //         loaded: false,
-  //         fetching: false,
-  //       }),
-  //     )
-  // }
+// componentWillUnmount() {
+//   this._isMounted = false
+// }
 
-  // safeSetState(...args) {
-  //   this._isMounted && this.setState(...args)
-  // }
+// query() {
+//   this.setState({fetching: true})
+//   const client = this.context
+//   client
+//     .request(this.props.query, this.props.variables)
+//     .then(res =>
+//       this.safeSetState({
+//         data: this.props.normalize(res),
+//         error: null,
+//         loaded: true,
+//         fetching: false,
+//       }),
+//     )
+//     .catch(error =>
+//       this.safeSetState({
+//         error,
+//         data: null,
+//         loaded: false,
+//         fetching: false,
+//       }),
+//     )
+// }
 
-  render() {
-    return this.props.children(this.state)
-  }
-}
+// safeSetState(...args) {
+//   this._isMounted && this.setState(...args)
+// }
 
-export default Query
+//   render() {
+//     return this.props.children(this.state)
+//   }
+// }
